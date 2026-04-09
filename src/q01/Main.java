c2bf1d2212d9d0ff2c862e67c95869829bd2f5db
nomaanahmad071 <csjma21000030946@csjmu.ac.in>
2026-04-09 10:47:29 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,25 +1,37 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        // TODO: Print a hollow diamond pattern of half-height n using '*'
-        //
-        // Rules:
-        //   - Only border cells have '*', inside is spaces
-        //   - No trailing spaces on any line
-        //
-        // Input: 4
-        // Output:
-        //    *
-        //   * *
-        //  *   *
-        // *     *
-        //  *   *
-        //   * *
-        //    *
-
+        
+        for (int i = 0; i < n; i++) {
+            int spaces = n - 1 - i;
+            int width = 2 * i + 1;
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < spaces; j++) sb.append(' ');
+            if (width == 1) {
+                sb.append('*');
+            } else {
+                sb.append('*');
+                for (int j = 0; j < width - 2; j++) sb.append(' ');
+                sb.append('*');
+            }
+            System.out.println(sb.toString());
+        }
+        
+        for (int i = n - 2; i >= 0; i--) {
+            int spaces = n - 1 - i;
+            int width = 2 * i + 1;
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < spaces; j++) sb.append(' ');
+            if (width == 1) {
+                sb.append('*');
+            } else {
+                sb.append('*');
+                for (int j = 0; j < width - 2; j++) sb.append(' ');
+                sb.append('*');
+            }
+            System.out.println(sb.toString());
+        }
     }
 }
