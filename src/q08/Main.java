b41656141c6ba6e45c4f2c7a3fb0f064e6a4f282
nomaanahmad071 <csjma21000030946@csjmu.ac.in>
2026-04-09 10:50:39 +0530

    Update Main.java

--- src/q08/Main.java
+++ src/q08/Main.java
@@ -4,28 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[] st = new int[n];
+        int top = -1;
 
-        // TODO: Implement a stack using an array (no Java Stack class)
-        //       Process n operations:
-        //         PUSH x  → push integer x
-        //         POP     → pop top, print it; if empty print "EMPTY"
-        //         PEEK    → print top without removing; if empty print "EMPTY"
-        //         SIZE    → print current number of elements
-        //
-        // Input:
-        // 6
-        // PUSH 10
-        // PUSH 20
-        // PEEK
-        // POP
-        // POP
-        // POP
-        //
-        // Output:
-        // 20
-        // 20
-        // 10
-        // EMPTY
-
+        while (n-- > 0) {
+            String op = sc.next();
+            if (op.equals("PUSH")) {
+                st[++top] = sc.nextInt();
+            } else if (op.equals("POP")) {
+                System.out.println(top == -1 ? "EMPTY" : st[top--]);
+            } else if (op.equals("PEEK")) {
+                System.out.println(top == -1 ? "EMPTY" : st[top]);
+            } else if (op.equals("SIZE")) {
+                System.out.println(top + 1);
+            }
+        }
     }
 }
